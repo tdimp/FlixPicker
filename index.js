6b6c806cc6868f3f4cab358fe6b573f791ae6537
Thomas Dimperio <twdimperio@gmail.com>
2022-01-14 16:07:59 -0700

    add input functionality and API fetch request

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ document.addEventListener("DOMContentLoaded", () => {
     movieSubmit.addEventListener("submit", (e) => {
         e.preventDefault();
         let input = document.querySelector("input#title").value;
-        console.log(input)
+        
+        fetch(`http://www.omdbapi.com/?t=${input}&apikey=5a6e25f8`,
+        {
+            method: "GET"
+        })
+        .then(res => res.json())
+        .then(data => {
+            handleSearch(data)
+        })
     })
 })
+
+
+function handleSearch(movie) {
+    let card = document.createElement('div');
+    card.className = 'card';
+    document.querySelector("#unwatched").appendChild(card);
+    let h3 = document.createElement('h3');
+    let cover = document.createElement('img');
+
+    card.appendChild(h3);
+    card.appendChild(cover);
+
+    h3.innerText = `${movie.Title} (${movie.Year})`;
+    cover.src = movie.Poster;
+    cover.className = "poster"
+}
