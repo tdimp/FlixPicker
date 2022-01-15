fa87a95517a56f0000dcf65c2b29470f73dc72a8
Thomas Dimperio <twdimperio@gmail.com>
2022-01-15 14:53:09 -0700

    Change CSS file. Add handleWatch function with third event listener

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const movieSubmit = document.querySelector('#search')
-
+    const movieSubmit = document.querySelector('#search');
+    
     movieSubmit.addEventListener("submit", (e) => {
         e.preventDefault();
         let input = document.querySelector("input#title").value;
@@ -13,21 +13,36 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(data => {
             handleSearch(data)
         })
+        e.target.reset();
     })
+    
+    document.addEventListener('click', handleWatch)
 })
 
 
-function handleSearch(movie) {
+function handleSearch(movie, queue) {
     let card = document.createElement('div');
     card.className = 'card';
     document.querySelector("#unwatched").appendChild(card);
     let h3 = document.createElement('h3');
     let cover = document.createElement('img');
+    let watchButton = document.createElement('button')
 
     card.appendChild(h3);
     card.appendChild(cover);
+    card.appendChild(watchButton);
 
     h3.innerText = `${movie.Title} (${movie.Year})`;
     cover.src = movie.Poster;
     cover.className = "poster"
+    watchButton.className = "button"
+    watchButton.id = "watch-button"
+    watchButton.innerText = "Watched?"
+}
+
+function handleWatch(e) {
+    // use .appendTo(#watched) or .prependTo(#watched)
+    if (e.target.id === "watch-button") {
+        document.getElementById("watched").appendChild(e.target.parentElement)
+    }
 }
